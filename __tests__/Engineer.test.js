e9b86fb76994312ccb0f89d885adf60aa2280cf6
Erica <erica.trenholm48@gmail.com>
2022-03-23 21:36:19 -0400

    create engineer object

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,40 @@
+const Engineer = require('../lib/Engineer');
+
+test('creates a engineer object', () => {
+    const engineer = new Engineer('Bob','1','bob@gmail.com','bobhub');
+
+    expect(engineer.name).toBe('Bob');
+    expect(engineer.id).toBe('1')
+    expect(engineer.email).toBe('bob@gmail.com')
+    expect(engineer.gitHub).toBe('bobhub')
+});
+
+test("gets engineer's name", () => {
+    const engineer = new Engineer('Bob','1','bob@gmail.com','bobhub');
+
+    expect(engineer.getName()).toBe('Bob')
+});
+
+test("gets engineer's id number", () => {
+    const engineer = new Engineer('Bob','1','bob@gmail.com','bobhub');
+
+    expect(engineer.getId()).toBe('1')
+});
+
+test("gets engineer's email address", () => {
+    const engineer = new Engineer('Bob','1','bob@gmail.com','bobhub');
+
+    expect(engineer.getEmail()).toBe('bob@gmail.com')
+});
+
+test("gets engineer's github's username", () => {
+    const engineer = new Engineer('Bob','1','bob@gmail.com','bobhub');
+
+    expect(engineer.getEmail()).toBe('bob@gmail.com')
+});
+
+test("gets engineer's role or job title", () => {
+    const engineer = new Engineer('Bob','1','bob@gmail.com','bobhub');
+
+    expect(engineer.getRole()).toBe('Engineer')
+});
