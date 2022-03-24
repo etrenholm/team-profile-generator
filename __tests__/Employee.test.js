8fac8858cce501d4749c591b3bb387bd3416acec
Erica <erica.trenholm48@gmail.com>
2022-03-23 20:31:38 -0400

    refactored employee object

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -12,22 +12,22 @@ test("gets employee's name", () => {
     const employee = new Employee('Erica','1','erica@gmail.com');
 
     expect(employee.getName()).toBe('Erica')
-})
+});
 
 test("gets employee's id number", () => {
     const employee = new Employee('Erica','1','erica@gmail.com');
 
     expect(employee.getId()).toBe('1')
-})
+});
 
 test("gets employee's email address", () => {
     const employee = new Employee('Erica','1','erica@gmail.com');
 
     expect(employee.getEmail()).toBe('erica@gmail.com')
-})
+});
 
 test("gets employee's role or job title", () => {
     const employee = new Employee('Erica','1','erica@gmail.com');
 
     expect(employee.getRole()).toBe('Employee')
-})
+});
