920220490825814e574f447014c87fa2fcb2641e
Erica <erica.trenholm48@gmail.com>
2022-03-23 21:47:19 -0400

    created intern object

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,40 @@
+const Intern = require('../lib/Intern');
+
+test('creates an intern object', () => {
+    const intern = new Intern('Mary','1','mary@gmail.com','Uconn');
+
+    expect(intern.name).toBe('Mary');
+    expect(intern.id).toBe('1')
+    expect(intern.email).toBe('mary@gmail.com')
+    expect(intern.school).toBe('Uconn')
+});
+
+test("gets intern's name", () => {
+    const intern = new Intern('Mary','1','mary@gmail.com','Uconn');
+
+    expect(intern.getName()).toBe('Mary')
+});
+
+test("gets intern's id number", () => {
+    const intern = new Intern('Mary','1','mary@gmail.com','Uconn');
+
+    expect(intern.getId()).toBe('1')
+});
+
+test("gets intern's email address", () => {
+    const intern = new Intern('Mary','1','mary@gmail.com','Uconn');
+
+    expect(intern.getEmail()).toBe('mary@gmail.com')
+});
+
+test("gets intern's school", () => {
+    const intern = new Intern('Mary','1','mary@gmail.com','Uconn');
+
+    expect(intern.getSchool()).toBe('Uconn')
+});
+
+test("gets intern's role or job title", () => {
+    const intern = new Intern('Mary','1','mary@gmail.com','Uconn');
+
+    expect(intern.getRole()).toBe('Intern')
+});
